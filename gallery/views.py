16bd379718b74a8ab95fdc0d2f7dd79bec4bcb84
James Kageni <james.kageni@gmail.com>
2018-09-27 15:33:59 +0300

    Added the landing template

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
-def welcome(request):
-    return HttpResponse("welcome to pictorial")
+def landing(request):
+    return render(request, "landing.html")
