ba3e88afb65c574e515416a8889414a6b8931d1d
James Kageni <james.kageni@gmail.com>
2018-09-29 14:32:25 +0300

    debug the return of name of pic

--- gallery/models.py
+++ gallery/models.py
@@ -37,14 +37,14 @@ class Picture(models.Model):
     name=models.CharField(max_length=30)
     image=models.ImageField(upload_to='images/')
     pub_date = models.DateTimeField(auto_now_add=True)
-    location = models.ForeignKey(Location)
+    location = models.ForeignKey(Location, default=1)
     category = models.ManyToManyField(Category)
 
     class meta:
         ordering = ['name']
 
     def __str__(self):
-        return self.title
+        return self.name
 
     def save_picture(self):
         # saves the picture
