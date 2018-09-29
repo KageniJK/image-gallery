dca496ee4205976d7553f904cd59e5a3b4a57fe6
James Kageni <james.kageni@gmail.com>
2018-09-29 19:51:42 +0300

    fixed the navbar in non-index pages and renamed landing to index

--- gallery/views.py
+++ gallery/views.py
@@ -7,26 +7,28 @@ from .models import Picture, Location
 def landing(request):
     pics = Picture.all_pics()
     locations = Location.objects.all()
-    return render(request, "landing.html", {'pics': pics, 'locations': locations})
+    return render(request, "index.html", {'pics': pics, 'locations': locations})
 
 
 def search_results(request):
+    locations = Location.objects.all()
     if 'pic' in request.GET and request.GET['pic']:
         search_term = request.GET.get('pic')
         searched_pics = Picture.search_picture(search_term)
         message = f'{search_term}'
 
-        return render(request, 'search.html', {'message': message, 'pics': searched_pics})
+        return render(request, 'search.html', {'message': message, 'pics': searched_pics, 'locations': locations})
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'search.html', {'message': message})
+        return render(request, 'search.html', {'message': message, 'locations': locations})
 
 
 def location(request, location):
+    locations = Location.objects.all()
     try:
         pics = Picture.filter_by_loc(location)
     except Picture.DoesNotExist:
         raise Http404()
 
-    return render(request, 'location.html', {'pics': pics})
+    return render(request, 'location.html', {'pics': pics, 'locations': locations})
