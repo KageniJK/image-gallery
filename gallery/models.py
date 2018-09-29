6ff0da334238f7f82dfed6a4cf001789bcc29d51
James Kageni <james.kageni@gmail.com>
2018-09-29 14:10:49 +0300

    added the methods to the models

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    """
+    class that defines the locations to be tagged on the images
+    """
+
+    location=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.location
+
+    def save_location(self):
+        self.save()
+
+
+class Category(models.Model):
+    """
+    class that defines the categories that the images will fit under
+    """
+
+    category = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category
+
+    def save_category(self):
+        self.save()
+
+
+class Picture(models.Model):
+    """
+    class that defines the images
+    """
+
+    name=models.CharField(max_length=30)
+    image=models.ImageField(upload_to='images/')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey(Location)
+    category = models.ManyToManyField(Category)
+
+    class meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.title
+
+    def save_picture(self):
+        # saves the picture
+        self.save()
+
+    @classmethod
+    def delete_picture(cls,id):
+        # deletes the picture from database
+        pic = cls.objects.filter(id=id)
+        pic.delete()
+
+    @classmethod
+    def get_pic_by_id(cls,id):
+        # gets a specific picture
+        return cls.objects.filter(id=id)
+
+    @classmethod
+    def search_picture(cls, search_term):
+        # returns a searched image by category
+        return cls.objects.filter(category__icontains=search_term)
+
+    @classmethod
+    def filter_by_loc(cls,location):
+        # returns all the pictures from a specific location
+        return cls.objects.filter(location=location)
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Picture, Location, Category
+import datetime as dt
 
-# Create your tests here.
+
+class PictureTestClass(TestCase):
+    """
+    class that tests the Picture class
+    """
+
+    def setUp(self):
+        #setting up instance
+        self.one = Picture(name = 'today')
