7b8ebf0b0458d506e49f9bfd536add9df13cc81d
James Kageni <james.kageni@gmail.com>
2018-10-01 08:32:22 +0300

    deployment to heroku

--- README.md
+++ README.md
@@ -8,9 +8,10 @@ This is an app that acts as my personal online photo gallery.
 ---
 This project has utilized the following technologies:
 1. Python 3.6
-2. Flask
+2. Django
 3. Bootstrap
 4. SQL
+5. Fontawesome
 
 ---
 ## Prerequisites
@@ -22,17 +23,16 @@ You need the following to make edits to the repository:
 
 
 ## Setup Instructions:
-Just in case you want to set up your own blog with your styles
+Just in case you want to set up your own gallery with your styles
   1. Copy the clone link provided in the repository
   2. Start your command line
   3. Run the git clone command with the copied links
   4. Make changes to the remote(s) used to point to your repository
-  5. Create a virtual environment and install all the files in the requirements.txt
-  6. Setup your config file with your secret key for use in the forms
-  7. make your edits and view them by running the python local server
+  5. Create a virtual environment and install all the files in the requirements.txt (pip install -r requirements.txt)
+  7. make your edits and view them by running the python local server(python3.6 manage.py runserver)
 
 ---
 
-A live view of the repository can be found at [Heroku](https://mortus.herokuapp.com/)
+A live view of the repository can be found at [Heroku](https://hall-of-james.herokuapp.com/)
 ___
 This is licensed under an MIT license that can be found at: [License](LICENSE)
--- pictorial/settings.py
+++ pictorial/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
+from decouple import config, Csv
+MODE=config("MODE", default="dev")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,9 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'daz56vd*45@c-rp-+!o6&*n_xquw$7syd@l9umle()wf9^0i6g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 
 # Application definition
@@ -42,6 +43,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -76,14 +80,29 @@ WSGI_APPLICATION = 'pictorial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pictorial',
-        'USER': 'qagz',
-        'PASSWORD': 'password',
+if config('MODE') == 'dev':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': '',
+        }
+
     }
-}
+# production
+else:
+    DATABASES = {
+        'default': dj_database_url.config(default=config('DATABASE_URL')
+        )
+    }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Password validation
@@ -121,11 +140,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
